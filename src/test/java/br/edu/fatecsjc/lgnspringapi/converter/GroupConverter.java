1e74218a1c8c81a21d6e978e01721c6b3b8f4d1b
BrunoSerpa <serpabruno2005@gmail.com>
2025-04-24 20:13:57 -0300

    Adicionei os testes para os conversores Converter e GroupConverter

---
+++ src/test/java/br/edu/fatecsjc/lgnspringapi/converter/GroupConverter.java
@@ -0,0 +1,77 @@
+package br.edu.fatecsjc.lgnspringapi.converter;
+
+import br.edu.fatecsjc.lgnspringapi.dto.GroupDTO;
+import br.edu.fatecsjc.lgnspringapi.entity.Group;
+import br.edu.fatecsjc.lgnspringapi.entity.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupConverterTest {
+
+    private GroupConverter groupConverter;
+
+    @BeforeEach
+    void setUp() {
+        groupConverter = new GroupConverter(new ModelMapper());
+    }
+
+    @Test
+    @DisplayName("Should convert GroupDTO to Group entity")
+    void testConvertToEntity() {
+        GroupDTO groupDTO = new GroupDTO(1L, "Group A", List.of());
+        Group group = groupConverter.convertToEntity(groupDTO);
+
+        assertNotNull(group);
+        assertEquals("Group A", group.getName());
+    }
+
+    @Test
+    @DisplayName("Should convert Group entity to GroupDTO")
+    void testConvertToDto() {
+        Group group = Group.builder()
+                .id(1L)
+                .name("Group A")
+                .members(List.of(Member.builder().id(1L).name("Alice").build()))
+                .build();
+
+        GroupDTO groupDTO = groupConverter.convertToDto(group);
+
+        assertNotNull(groupDTO);
+        assertEquals("Group A", groupDTO.getName());
+        assertEquals(1, groupDTO.getMembers().size());
+        assertEquals("Alice", groupDTO.getMembers().get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Should convert list of GroupDTO to list of Group entities")
+    void testConvertToEntityList() {
+        GroupDTO groupDTO = new GroupDTO(1L, "Group A", List.of());
+        List<Group> groups = groupConverter.convertToEntity(List.of(groupDTO));
+
+        assertNotNull(groups);
+        assertEquals(1, groups.size());
+        assertEquals("Group A", groups.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Should convert list of Group entities to list of GroupDTOs")
+    void testConvertToDtoList() {
+        Group group = Group.builder()
+                .id(1L)
+                .name("Group A")
+                .members(List.of(Member.builder().id(1L).name("Alice").build()))
+                .build();
+
+        List<GroupDTO> groupDTOs = groupConverter.convertToDto(List.of(group));
+
+        assertNotNull(groupDTOs);
+        assertEquals(1, groupDTOs.size());
+        assertEquals("Group A", groupDTOs.get(0).getName());
+    }
+}
